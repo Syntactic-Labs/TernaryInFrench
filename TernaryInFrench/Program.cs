702e793dcc90dbbd8d2439e716a7d34e66d6561c
Michael Walker <walkerrepair255@yahoo.com>
2021-10-21 20:44:19 -0400

    Add project files.

---
+++ TernaryInFrench/Program.cs
@@ -0,0 +1,26 @@
+﻿//checks for french
+#define xFrench
+using System;
+
+namespace TernaryInFrench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Two var for my Ternary
+            var yes = "Yes";
+            var no = "No";
+//French Ternary response
+#if French
+            yes = "Oui";
+            no = "Non";
+
+#endif
+            var boolean = true;
+
+            
+            Console.WriteLine($"boolean is {boolean}; text is {(boolean ? yes : no)}");
+        }
+    }
+}
